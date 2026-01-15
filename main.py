d6dd19213c041091b34758e62698ce3b93421155
alxk0909 <131524744+alxk0909@users.noreply.github.com>
2026-01-15 21:53:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import logging
-
-from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.client.Extension import Extension
-from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
-from ulauncher.api.shared.action.RunScriptAction import RunScriptAction
+from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RunScriptAction import RunScriptAction
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 
 logger = logging.getLogger(__name__)
-
 
 class StretchlyExtension(Extension):
     def __init__(self):
         super(StretchlyExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
-
 
 class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
@@ -30,29 +27,30 @@ class KeywordQueryEventListener(EventListener):
         # script: the actual shell command to run
         commands = [
             {
-                "key": "reset",
-                "name": "Reset Stretchly",
-                "description": "Resets breaks (flatpak run net.hovancik.Stretchly reset)",
-                "script": "flatpak run net.hovancik.Stretchly reset",
+                'key': 'reset',
+                'name': 'Reset Stretchly',
+                'description': 'Resets breaks (flatpak run net.hovancik.Stretchly reset)',
+                'script': 'flatpak run net.hovancik.Stretchly reset'
             },
             {
-                "key": "toggle",
-                "name": "Toggle Stretchly",
-                "description": "Pauses/Resumes breaks (flatpak run net.hovancik.Stretchly toggle)",
-                "script": "flatpak run net.hovancik.Stretchly toggle",
-            },
+                'key': 'toggle',
+                'name': 'Toggle Stretchly',
+                'description': 'Pauses/Resumes breaks (flatpak run net.hovancik.Stretchly toggle)',
+                'script': 'flatpak run net.hovancik.Stretchly toggle'
+            }
         ]
 
         # Filter items: show all if query is empty, otherwise match start of string
         for cmd in commands:
-            if cmd["key"].startswith(query):
-                items.append(
-                    ExtensionResultItem(
-                        icon="images/icon.svg",
-                        name=cmd["name"],
-                        description=cmd["description"],
-                        on_enter=RunScriptAction(cmd["script"], []),
-                    )
-                )
+            if cmd['key'].startswith(query):
+                items.append(ExtensionResultItem(
+                    icon='images/icon.svg',
+                    name=cmd['name'],
+                    description=cmd['description'],
+                    on_enter=RunScriptAction(cmd['script'], [])
+                ))
 
         return RenderResultListAction(items)
+
+if __name__ == '__main__':
+    StretchlyExtension().run()
